477a6298387dd68e57efc64e52b9ba6326ca809c
AdityaNijave <adityanijave09@gmail.com>
2021-08-20 17:15:03 +0530

    guess the num

--- Class Projects/gussing the number by user.py
+++ Class Projects/gussing the number by user.py
@@ -2,12 +2,15 @@ import random
 c = 1
 while True:
     computer = random.randint(0, 10)
-    user = int(input("enter your guessed number from 0 to 10 : "))
 
-    if computer == user :
-        print(f"congo! \nComputer choose number : {computer} \nyou guess the number : {user}")
-        break
-    else :
-        print(f"try again \nComputer choose number : {computer} \nyou guess the number : {user}")
-        c+=1
+    user = (input("enter your guessed number from 0 to 10 : "))
+    if user.isdigit():
+        if computer == int(user) :
+            print(f"congo! you won \nComputer choose number : {computer} \nyou guess the number : {user}")
+            break
+        elif computer != int(user) :
+            print(f"you lose, try again \nComputer choose number : {computer} \nyou guess the number : {user}\n")
+            c+=1
+    else:
+        print('please enter valid entry!\n')
 print(f"Total try is {c}")
