3da0c410a12cf2a4fec14ab0e907eaa167efc4b9
AdityaNijave <adityanijave09@gmail.com>
2021-08-20 18:16:42 +0530

    multiplies all element inside the list

---
+++ Classwork Codes/getting product of all element in a list.py
@@ -0,0 +1,5 @@
+numberlist = [1,2,3,4,5,6,7,8,9]
+numberproduct = 1
+for number in numberlist :
+    numberproduct *= number
+print(f"The product of all numbers present in a list is {numberproduct}")
