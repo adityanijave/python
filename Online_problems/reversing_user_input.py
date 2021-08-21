fad8fcbf5fc753227ebae61671d37381e1f3fcc3
AdityaNijave <adityanijave09@gmail.com>
2021-08-21 20:33:39 +0530

    updating string format as required

---
+++ Online_problems/reversing_user_input.py
@@ -0,0 +1,8 @@
+# 5. Write a Python program which accepts the user's first and last name and
+# print them in reverse order with a space between them. Go to the editor
+# Click me to see the sample solution
+
+
+
+user_first_name,user_last_name= input("Enter your first name : "),input("Enter your last name : ")
+print(f"Your name look's like {user_first_name} {user_last_name}\nYour updated name look's like {user_last_name} {user_first_name}")
