2399a5ebbe32788cf7fc4589becd5645c63e93e4
AdityaNijave <adityanijave09@gmail.com>
2021-08-22 10:05:26 +0530

    display the first and last colors from the given list

---
+++ Online_problems/display zero and last based index element.py
@@ -0,0 +1,7 @@
+#
+# 8. Write a Python program to display the first and last colors from the following list. Go to the editor
+# color_list = ["Red","Green","White" ,"Black"]
+# Click me to see the sample solution
+
+color_list = ["Red","Green","White" ,"Black"]
+print(f"The first color from the given list is '{color_list[0]}'\nThe last color from the given list is '{color_list[-1]}'")
