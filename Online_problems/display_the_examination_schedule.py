f143a75acddff1529bb05ba322c98fdb40fae5d4
AdityaNijave <adityanijave09@gmail.com>
2021-08-22 12:06:37 +0530

    display the examination schedule as per given date

---
+++ Online_problems/display_the_examination_schedule.py
@@ -0,0 +1,7 @@
+# 9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+# exam_st_date = (11, 12, 2014)
+# Sample Output : The examination will start from : 11 / 12 / 2014
+
+exam_st_date = (11, 12, 2014)
+print(type(exam_st_date))
+print(f"The examination will start from : {exam_st_date[0]} / {exam_st_date[1]} / {exam_st_date[2]}")
