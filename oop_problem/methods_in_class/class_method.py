0415df468d878be580322278a33df9edcd928b12
AdityaNijave <adityanijave09@gmail.com>
2021-09-03 05:00:55 +0530

    one of the class method

---
+++ oop_problem/methods_in_class/class_method.py
@@ -0,0 +1,8 @@
+class test:
+    s = "this is class variable"
+    @classmethod
+    def show(cls):
+        print(cls.s)
+
+ss = test()
+ss.show()
---
+++ oop_problem/methods_in_class/static_method.py
@@ -0,0 +1,7 @@
+class test:
+    @staticmethod
+    def disop():
+        print("This is static methods!")
+
+ram = test()
+ram.disop()
