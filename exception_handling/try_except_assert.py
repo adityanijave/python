a1b053f36a43df113f352a0ef6881ba60caeaded
AdityaNijave <adityanijave09@gmail.com>
2021-09-14 12:08:23 +0530

    exception handling by using try and except and assert block

---
+++ exception_handling/try_except_assert.py
@@ -0,0 +1,11 @@
+number = int(input("enter a number between 5 and 10 : "))
+'''
+syntax :
+      assert condition , message 
+'''
+
+try:
+    assert 5 <= number <= 10 , "Number is not in between 5 and 10"
+    print("Yes! The number you entered is correct .")
+except AssertionError as e:
+    print(e)
