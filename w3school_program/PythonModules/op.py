d141e169b70a805a9b6614cc6db673cc4a90a2e8
AdityaNijave <adityanijave09@gmail.com>
2022-01-08 19:48:21 +0530

    creating and using the user defined modules

--- w3school_program/PythonModules/op.py
+++ w3school_program/PythonModules/op.py
@@ -1,1 +1,11 @@
+import calculater
+user = input("enter operator +,-,* :")
+n1 = int(input("enter number 1 : "))
+n2 = int(input("enter number 2 : "))
 
+if user == "+":
+    print(calculater.add(n1, n2))
+elif user == "-":
+    print(calculater.sub(n1, n2))
+elif user == "*":
+    print(calculater.multi(n1, n2))
